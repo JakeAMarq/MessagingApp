0733e121515c1a640a61fb6c3c042631f257f303
JakeAMarq <jacobmarquardt1@gmail.com>
2020-04-29 16:16:38 -0700

    Added sign in page, registration page, main activity with bottom nav (and the placeholder pages) for home, conversations, contacts, and weather. Since we don't have back end, the sign in button just bypasses signing in and sends a fake email and jwt to main activity

---
+++ app/src/main/java/edu/uw/tcss450/team4projectclient/ui/auth/register/RegisterFragment.java
@@ -0,0 +1,149 @@
+package edu.uw.tcss450.team4projectclient.ui.auth.register;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import edu.uw.tcss450.team4projectclient.databinding.FragmentRegisterBinding;
+import edu.uw.tcss450.team4projectclient.utils.PasswordValidator;
+
+import static edu.uw.tcss450.team4projectclient.utils.PasswordValidator.*;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RegisterFragment extends Fragment {
+
+    private FragmentRegisterBinding binding;
+
+    private RegisterViewModel mRegisterModel;
+
+    private PasswordValidator mNameValidator = checkPwdLength(1);
+
+    private PasswordValidator mEmailValidator = checkPwdLength()
+            .and(checkExcludeWhiteSpace())
+            .and(checkPwdSpecialChar("@"));
+
+    private PasswordValidator mPassWordValidator =
+            checkClientPredicate(pwd -> pwd.equals(binding.editPassword2.getText().toString()))
+                    .and(checkPwdLength(7))
+                    .and(checkPwdSpecialChar())
+                    .and(checkExcludeWhiteSpace())
+                    .and(checkPwdDigit())
+                    .and(checkPwdLowerCase().or(checkPwdUpperCase()));
+
+    public RegisterFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mRegisterModel = new ViewModelProvider(getActivity())
+                .get(RegisterViewModel.class);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        this.binding = FragmentRegisterBinding.inflate(inflater);
+        return this.binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        this.binding.buttonRegister.setOnClickListener(this::attemptRegister);
+
+        mRegisterModel.addResponseObserver(
+                getViewLifecycleOwner(),
+                this::observeResponse
+        );
+
+    }
+
+    private void observeResponse(final JSONObject response) {
+        if (response.length() > 0) {
+            if (response.has("code")) {
+                try {
+                    binding.editEmail.setError(
+                            "Error Authenticating: " +
+                                    new JSONObject(
+                                            response.getString("data").
+                                                    replace("'", "\"")
+                                    ).getString("message"));
+                } catch (JSONException e) {
+                    Log.e("JSON Parse Error", e.getMessage());
+                }
+            } else {
+                navigateToLogin();
+            }
+        } else {
+            Log.d("JSON Response", "No Response");
+        }
+    }
+
+    private void attemptRegister(final View button) {
+        validateFirst();
+    }
+
+    private void validateFirst() {
+        mNameValidator.processResult(
+                mNameValidator.apply(binding.editFirst.getText().toString().trim()),
+                this::validateLast,
+                result -> binding.editFirst.setError("Please enter a valid first name."));
+    }
+
+    private void validateLast() {
+        mNameValidator.processResult(
+                mNameValidator.apply(binding.editLast.getText().toString().trim()),
+                this::validateEmail,
+                result -> binding.editLast.setError("Please enter a valid last name."));
+    }
+
+    private void validateEmail() {
+        mEmailValidator.processResult(
+                mEmailValidator.apply(binding.editEmail.getText().toString().trim()),
+                this::validatePassword,
+                result -> binding.editEmail.setError("Please enter a valid Email address."));
+    }
+
+    private void validatePassword() {
+        mPassWordValidator.processResult(
+                mPassWordValidator.apply(binding.editPassword1.getText().toString()),
+                this::verifyAuthWithServer,
+                result -> binding.editPassword1.setError("Please enter a valid Password."));
+    }
+
+    private void verifyAuthWithServer() {
+        mRegisterModel.connect(
+                binding.editFirst.getText().toString(),
+                binding.editLast.getText().toString(),
+                binding.editEmail.getText().toString(),
+                binding.editPassword1.getText().toString());
+    }
+
+    private void navigateToLogin() {
+        RegisterFragmentDirections.ActionRegisterFragmentToSignInFragment directions =
+                RegisterFragmentDirections.actionRegisterFragmentToSignInFragment();
+
+        directions.setEmail(binding.editEmail.getText().toString());
+        directions.setPassword(binding.editPassword1.getText().toString());
+
+        Navigation.findNavController(getView()).navigate(directions);
+
+    }
+}
---
+++ app/src/main/java/edu/uw/tcss450/team4projectclient/ui/auth/signin/SignInFragment.java
@@ -0,0 +1,145 @@
+package edu.uw.tcss450.team4projectclient.ui.auth.signin;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import edu.uw.tcss450.team4projectclient.databinding.FragmentSignInBinding;
+import edu.uw.tcss450.team4projectclient.utils.PasswordValidator;
+import static edu.uw.tcss450.team4projectclient.utils.PasswordValidator.*;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SignInFragment extends Fragment {
+
+    private FragmentSignInBinding binding;
+    private SignInViewModel mSignInModel;
+
+    private PasswordValidator mEmailValidator = checkPwdLength(2)
+            .and(checkExcludeWhiteSpace())
+            .and(checkPwdSpecialChar("@"));
+
+    private PasswordValidator mPassWordValidator = checkPwdLength(1)
+            .and(checkExcludeWhiteSpace());
+
+    public SignInFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mSignInModel = new ViewModelProvider(getActivity()).get(SignInViewModel.class);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        this.binding = FragmentSignInBinding.inflate(inflater, container, false);
+        return this.binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+
+//        this.binding.buttonLogIn.setOnClickListener(this::attemptLogin);
+        this.binding.buttonLogIn.setOnClickListener(this::byPassLogin);
+        this.binding.buttonRegister.setOnClickListener(button -> navigateToRegisterFragment());
+
+        mSignInModel.addResponseObserver(
+                getViewLifecycleOwner(),
+                this::observeResponse
+        );
+
+        SignInFragmentArgs args = SignInFragmentArgs.fromBundle(getArguments());
+        binding.editEmail.setText(args.getEmail().equals("default") ? "" : args.getEmail());
+        binding.editPassword.setText(args.getPassword().equals("default") ? "" : args.getPassword());
+    }
+
+    private void byPassLogin(final View button) {
+        navigateToMainActivity("fakeemail@email.email", "fakeJwt");
+    }
+
+    private void attemptLogin(final View button) {validateEmail();}
+
+    private void validateEmail() {
+        mEmailValidator.processResult(
+                mEmailValidator.apply(binding.editEmail.getText().toString().trim()),
+                this::validatePassword,
+                result -> binding.editEmail.setError("Please enter a valid Email address."));
+    }
+
+    private void validatePassword() {
+        mPassWordValidator.processResult(
+                mPassWordValidator.apply(binding.editPassword.getText().toString()),
+                this::verifyAuthWithServer,
+                result -> binding.editPassword.setError("Please enter a valid Password."));
+    }
+
+    private void verifyAuthWithServer() {
+        mSignInModel.connect(
+                binding.editEmail.getText().toString(),
+                binding.editPassword.getText().toString());
+    }
+
+    private void navigateToRegisterFragment() {
+        Navigation.findNavController(getView()).navigate(SignInFragmentDirections.actionSignInFragmentToRegisterFragment());
+    }
+
+    private void navigateToMainActivity(String email, String jwt) {
+        SignInFragmentDirections.ActionSignInFragmentToMainActivity directions =
+                SignInFragmentDirections.actionSignInFragmentToMainActivity(email, jwt);
+        Navigation.findNavController(getView()).navigate(directions);
+    }
+
+    /**
+     * An observer on the HTTP Response from the web server. This observer should be
+     * attached to SignInViewModel.
+     *
+     * @param response the Response from the server
+     */
+    private void observeResponse(final JSONObject response) {
+        if (response.length() > 0) {
+            if (response.has("code")) {
+                try {
+                    binding.editEmail.setError(
+                            "Error Authenticating: " +
+                                    new JSONObject(response.getString("data").
+                                            replace("'", "\""))
+                                            .getString("message"));
+
+                } catch (JSONException e) {
+//                    Log.e("test", response.toString());
+                    Log.e("JSON Parse Error", e.getMessage());
+                }
+            } else {
+                try {
+                    navigateToMainActivity(
+                            binding.editEmail.getText().toString(),
+                            response.getString("token"));
+                } catch (JSONException e) {
+                    Log.e("JSON Parse Error", e.getMessage());
+                }
+            }
+        } else {
+            Log.d("JSON Response", "No Response");
+        }
+    }
+
+}
