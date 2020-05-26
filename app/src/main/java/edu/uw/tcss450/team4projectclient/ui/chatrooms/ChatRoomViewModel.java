70e102f8d89a66b301111e4825e4a6e5fb50d2ad
JakeAMarq <jacobmarquardt1@gmail.com>
2020-05-26 07:23:10 -0700

    Added function to remove user from chat room

--- app/src/main/java/edu/uw/tcss450/team4projectclient/ui/chatrooms/ChatRoomViewModel.java
+++ app/src/main/java/edu/uw/tcss450/team4projectclient/ui/chatrooms/ChatRoomViewModel.java
@@ -92,6 +92,44 @@ public class ChatRoomViewModel extends AndroidViewModel {
     /**
      *
      */
+    public void leaveChatRoom(final int chatid, final String email, final String jwt) {
+        String url = getApplication().getResources().getString(R.string.base_url) +
+                "chats/" +
+                chatid + "/" +
+                email;
+
+        Request request = new JsonObjectRequest(
+                Request.Method.POST,
+                url,
+                null, //no body for this get request
+                response -> {
+                    List<Integer> chatIds = mChatIds.getValue();
+                    chatIds.remove(Integer.valueOf(chatid));
+                    mChatIds.setValue(chatIds);
+                },
+                this::handleError) {
+
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+                // add headers <key,value>
+                headers.put("Authorization", jwt);
+                return headers;
+            }
+        };
+
+        request.setRetryPolicy(new DefaultRetryPolicy(
+                10_000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        //Instantiate the RequestQueue and add the request to the queue
+        RequestQueueSingleton.getInstance(getApplication().getApplicationContext())
+                .addToRequestQueue(request);
+    }
+
+    /**
+     *
+     */
     public void addChatRoom(final String chatRoomName, final String jwt) {
         String url = getApplication().getResources().getString(R.string.base_url) +
                 "chats/";
